11d3e31944d05fabb5b1ddfaf12e699e4622ba3f
Dargons10 <jdamr2p@gmail.com>
2023-08-29 08:27:59 +0200

    Remove mediaextractor from mocha.mk

--- mocha.mk
+++ mocha.mk
@@ -323,7 +323,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor seccomp policy files for media components:
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
     
 # Wifi
 PRODUCT_COPY_FILES += \

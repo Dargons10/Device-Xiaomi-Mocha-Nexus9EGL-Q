f80e76915d4b0cca43148c378580f96f1a721287
dargons10 <jdamr2p@gmail.com>
2023-08-31 19:32:35 +0200

    Fix all patches and Root Added

--- patches/check.sh
+++ patches/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="frameworks/native frameworks/base external/selinux system/core system/extra"
+dirs="frameworks/native external/selinux system/core system/extras"
 
 
 RED='\033[0;31m'
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="frameworks/native frameworks/base external/selinux system/core system/extra"
+dirs="frameworks/native  external/selinux system/core system/extras"
 
 
 for dir in $dirs ; do

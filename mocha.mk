a20a3290d6b060be6eee0af2aeafc2e873d21ffa
380583067 <www.1064326656@qq.com>
2025-07-17 02:35:32 +0000

    Low memory control update

--- mocha.mk
+++ mocha.mk
@@ -204,15 +204,7 @@ PRODUCT_COPY_FILES += \
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service    
-
-# Memory Optimizations
-PRODUCT_PROPERTY_OVERRIDES += \
-     ro.vendor.qti.am.reschedule_service=true \
-     ro.vendor.qti.sys.fw.use_trim_settings=true \
-     ro.vendor.qti.sys.fw.trim_empty_percent=50 \
-     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-     ro.vendor.qti.sys.fw.empty_app_percent=25
+    android.hardware.memtrack@1.0-service
 
 # NVIDIA
 PRODUCT_COPY_FILES += \

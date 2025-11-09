0e0f8e2be26ab10f7a1d57465090e66da55a0c2e
apple <105837597+380583067@users.noreply.github.com>
2025-11-09 11:10:35 +0000

    Add power.mocha.rc to Android.mkRestore CPU core control logic

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -87,12 +87,13 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := power.mocha.rc
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := power.mocha.rc
-#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := power.mocha.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := power.mocha.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := power.tn8.rc

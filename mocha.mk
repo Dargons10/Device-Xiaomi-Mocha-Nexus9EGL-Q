8503782f89e8a10b92be8841bbcdff70b022a054
apple <105837597+380583067@users.noreply.github.com>
2026-02-27 15:47:00 +0000

    Fix the audio amplifier function

--- mocha.mk
+++ mocha.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0 \
     android.hardware.audio@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.usb.default \
